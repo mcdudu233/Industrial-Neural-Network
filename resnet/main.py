523acfeaa0d0900a950b3a7ba755cb7995cc3d4b
dudu233 <1147793932@qq.com>
2024-03-16 16:22:13 +0800

    优化超参数

--- resnet/main.py
+++ resnet/main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     # 超参数
     learning_rate = 0.018  # 初始学习率
-    learning_factor = 0.93  # 学习率调整因子
+    learning_factor = 0.92  # 学习率调整因子
 
     model = resnet34(2)  # 使用34层的resnet模型 加上softmax
     criterion = nn.CrossEntropyLoss()  # 损失计算器 均方误差
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,  # 学习率调整器 ReduceLROnPlateau lr=lr*factor
                                                      mode='min',
                                                      factor=learning_factor,
-                                                     patience=60)
+                                                     patience=80)
 
     # 训练模型或者测试数据
     if IS_TRAIN:
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         train_data = data.get_train_data(IS_DEBUG)
         # 训练模型
         model.train()
-        train(train_data, model, criterion, optimizer, scheduler, epochs=5, IS_DEBUG=IS_DEBUG, IS_CUDA=IS_CUDA)
+        train(train_data, model, criterion, optimizer, scheduler, epochs=20, IS_DEBUG=IS_DEBUG, IS_CUDA=IS_CUDA)
         # 保存训练好的模型
         torch.save(model.state_dict(), MODEL_PATH)
     else:
